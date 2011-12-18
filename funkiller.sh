270ba87df4c0bcce10192088fe20f6c8d2d2e913
Josh Lapham <josh@joshlapham.com>
2011-12-19 00:35:37 +1100

    Added python version.

--- funkiller.sh
+++ funkiller.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-HostsFile = /etc/hosts
+# FUNKILLER
+# Disables popular time wasting websites to encourage productivity.
 
+# Path to hosts file.
+$HostsFile = /etc/hosts
+
+# Start of functions.
+enableFun {
+  echo "Enabling FunKiller .."
+  # cat sites to hosts file
+  echo "FunKiller enabled. Do some work!"
+}
+
+disableFun {
+  echo "Disabling FunKiller .."
+  # remove lines from hosts file
+  echo "FunKiller disabled."
+}
 
