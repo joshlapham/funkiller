3c2ea7f30924b9d2bb99f255ef27672c195fecd8
Josh Lapham <josh@joshlapham.com>
2011-12-19 02:08:42 +1100

    Updated shell script.

--- funkiller.sh
+++ funkiller.sh
@@ -3,19 +3,59 @@
 # FUNKILLER
 # Disables popular time wasting websites to encourage productivity.
 
+# VARIABLES
 # Path to hosts file.
-$HostsFile = /etc/hosts
+hostsFile=$hostsFile
+export hostsFile="/etc/hosts"
+# Path to sites file.
+sitesFile=$sitesFile
+export sitesFile="data.txt"
+# Path to tmp file.
+tmpFile=$tmpFile
+export tmpFile="data.tmp"
+# Menu choice.
+readChoice=$readChoice
 
-# Start of functions.
-enableFun {
+# FUNCTIONS
+# Enable FunKiller.
+enableFun ()
+{
   echo "Enabling FunKiller .."
   # cat sites to hosts file
   echo "FunKiller enabled. Do some work!"
 }
 
-disableFun {
+# Disable FunKiller.
+disableFun ()
+{
   echo "Disabling FunKiller .."
   # remove lines from hosts file
+  sed -e "s/127.0.0.1/#127.0.0.1/g" $sitesFile >> $tmpFile
+  #sudo cat $tmpFile >> $hostsFile
+  rm -f $tmpFile
   echo "FunKiller disabled."
 }
+
+echo "FUNKILLER"
+echo -n "Enable or disable?: "
+read -n 1 readChoice
+echo ""
+
+# Accept Y/N answers only.
+while [ "$readChoice" != y -a "$readChoice" != Y -a "$readChoice" != n -a "$readChoice" != N ]; do
+    /bin/echo -n "Choose Y or N: "
+    read -n 1 readChoice
+    /bin/echo ""
+    done
+
+case $readChoice in
+
+	        y|Y)    
+					enableFun
+	        		;;
+
+	        n|N)
+	                disableFun
+	                ;;
+	esac
 
