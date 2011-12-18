de0f5fe3c04818788bb264e9cdaaf9e979b7bcd0
Josh Lapham <josh@joshlapham.com>
2011-12-19 04:12:56 +1100

    Updated files.

--- funkiller.sh
+++ funkiller.sh
@@ -3,6 +3,15 @@
 # FUNKILLER
 # Disables popular time wasting websites to encourage productivity.
 
+# By Josh Lapham [josh@joshlapham.com]
+
+# DISCLAIMER: I don't take responsibility for any of this.
+
+# Misc notes:
+# * Must be run as sudo.
+# * Appends to end of /etc/hosts file.
+# * Enabling and disabling clears FunKiller section of hosts file with sed. Disable comments out hosts added by FunKiller.
+
 # VARIABLES
 # Directory where script resides.
 BASEDIR=$(dirname $0)
@@ -23,7 +32,9 @@ readChoice=$readChoice
 enableFun ()
 {
   echo "Enabling FunKiller .."
+  # Find FunKiller comment header & footer in hosts file and remove lines.
   sed -i '' "/FUNKILLER/,/FUNKILLER/d" $hostsFile
+  # Append sites.txt file to end of hosts file.
   cat $sitesFile >> $hostsFile
   echo "FunKiller enabled. Do some work!"
 }
@@ -32,9 +43,13 @@ enableFun ()
 disableFun ()
 {
   echo "Disabling FunKiller .."
+  # Find FunKiller comment header & footer in hosts file and remove lines.
   sed -i '' "/FUNKILLER/,/FUNKILLER/d" $hostsFile
+  # Comment out 127.0.0.1 lines in sites.txt file and write to a temp file.
   sed -e "s/127.0.0.1/#127.0.0.1/g" $sitesFile >> $tmpFile
+  # Write commented out lines from temp file to hosts file.
   cat $tmpFile >> $hostsFile
+  # Clean up and remove temp file.
   rm -f $tmpFile
   echo "FunKiller disabled."
 }
