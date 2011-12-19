ce093302d711f6f436eea09e97548b13ac3f9c87
Josh Lapham <josh@joshlapham.com>
2011-12-19 22:54:13 +1100

    Tested on Mac, updated sites.txt

--- funkiller.sh
+++ funkiller.sh
@@ -36,13 +36,16 @@ readChoice=$readChoice
 OS=`uname`
 
 # FUNCTIONS
-# Enable FunKiller
+# Enable Funkiller
 enableFun ()
 {
   echo "Enabling Funkiller .."
   # Find FunKiller comment header & footer in hosts file and remove lines.
   # Check and run different commands if OS is Mac.
   if [[ "$OS" == "Darwin" ]]; then
+    # TESTING
+    echo $OS
+    echo "Mac"
     sed -i '' "/FUNKILLER/,/FUNKILLER/d" $hostsFile
     # Append sites.txt file to end of hosts file.
     cat $sitesFile >> $hostsFile
@@ -50,6 +53,9 @@ enableFun ()
     dscacheutil -flushcache
     echo "Funkiller enabled. Do some work!"
   else
+    # TESTING
+    echo $OS
+    echo "Linux"
     sed -i "/FUNKILLER/,/FUNKILLER/d" $hostsFile
     # Append sites.txt file to end of hosts file.
     cat $sitesFile >> $hostsFile
@@ -57,13 +63,16 @@ enableFun ()
   fi
 }
 
-# Disable FunKiller
+# Disable Funkiller
 disableFun ()
 {
   echo "Disabling Funkiller .."
   # Find FunKiller comment header & footer in hosts file and remove lines.
   # Check and run different commands if OS is Mac.
   if [[ "$OS" == "Darwin" ]]; then
+    # TESTING
+    echo $OS
+    echo "Mac"
     sed -i '' "/FUNKILLER/,/FUNKILLER/d" $hostsFile
     # Comment out 127.0.0.1 lines in sites.txt file and write to a temp file.
     sed -e "s/127.0.0.1/#127.0.0.1/g" $sitesFile >> $tmpFile
@@ -75,6 +84,9 @@ disableFun ()
     rm -f $tmpFile
     echo "Funkiller disabled."
   else
+    # TESTING
+    echo $OS
+    echo "Linux"
     sed -i "/FUNKILLER/,/FUNKILLER/d" $hostsFile
     # Comment out 127.0.0.1 lines in sites.txt file and write to a temp file.
     sed -e "s/127.0.0.1/#127.0.0.1/g" $sitesFile >> $tmpFile
@@ -88,7 +100,7 @@ disableFun ()
 
 # START OF MAIN
 # Print choice
-echo "FunKiller"
+echo "Funkiller"
 echo -n "Enable or disable?: "
 read -n 1 readChoice
 echo ""
